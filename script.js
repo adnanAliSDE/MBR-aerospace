6849b8ca2f6b6d900c9add57dd224a96cb6e7981
adnanAliSDE <112294571+adnanAliSDE@users.noreply.github.com>
2022-10-10 22:43:13 +0530

    Test

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-var elem =document.documentElement;
 
-function openFullscreen() {
-alert("Main chala");
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.webkitRequestFullscreen) { /* Safari */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE11 */
-    elem.msRequestFullscreen();
-  }
-}
-alert("Hello");
